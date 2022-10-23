f2143938bd7b1479cca4b8734d7d96735e117aab
alfadelta10010 <alfadelta10010@gmail.com>
2022-10-23 22:42:50 +0530

    Fixed bot.py after Hacknight shenanigans

--- bot.py
+++ bot.py
@@ -1,53 +1,55 @@
-import discord #discord module is an API wrapper for discord written in python
+import discord # discord module is an API wrapper for discord written in python
 from discord.ext import commands 
 import os #allows us to interact with the operating system using python
 from dotenv import load_dotenv #used to access environment variables
 from discord_slash import SlashCommand  #discord_slash is a python API wrapper for discord interactions
 
-load_dotenv('.env') #loads the environment variables folder
-TOKEN = os.getenv('DISCORD_TOKEN') #returns value of the environment variable with key == DISCORD_TOKEN
+load_dotenv('.env') #loads the environment variables file
 
 client = commands.Bot(command_prefix='p!', help_command=None, intents=discord.Intents().all()) 
 #initialises a discord bot with the prefix for its commands set to p!
+
 #having no help command, and subscribed (listening) to all events
 slash = SlashCommand(client, sync_on_cog_reload = True, sync_commands=True)
 
-BOT_LOGS = 931523901731799080
-BOT_ROLE = 931588180174589983 #bot role id
+# .env summons
+BOT_LOGS = os.getenv('BOT_LOGS')   # Bot Logs channel ID
+BOTDEV_ROLE = os.getenv('BOTDEV_ROLE')   # Bot Dev' Role ID
+BOT_TOKEN = os.getenv('DISCORD_TOKEN') #returns value of the environment variable with key == DISCORD_TOKEN
 
 
-@client.command(aliases = ['loadit'])
-async def load(ctx, extension): #loads the cog
-    bot_devs = discord.utils.get(ctx.guild.roles, id=BOT_ROLE)
+@client.command(aliases = ['loadit'])   # Decorator that loads cog 
+async def load(ctx, extension):   # Function that loads cog
+    bot_devs = discord.utils.get(ctx.guild.roles, id=BOTDEV_ROLE)
     if(bot_devs in ctx.author.roles):
         try:
             client.load_extension(f"cogs.{extension}")
-            success = f"cogs.{extension} was loaded succesfully"
-            await ctx.channel.send(success)
-            await client.get_channel(BOT_LOGS).send(success)
-        except Exception as e: #except block to handle any exceptions encountered
-            await ctx.channel.send(e)
+            success_msg = f"cogs.{extension} was loaded succesfully"
+            await ctx.channel.send(success_msg)
+            await client.get_channel(BOT_LOGS).send(success_msg)
+        except Exception as err:   # Except block to handle any exceptions encountered
+            await ctx.channel.send(err)
     else:
-        await ctx.channel.send("Unauthorised")
+        await ctx.channel.send("Unauthorised. How did you find this wtf-")
 
 
-@client.command(aliases = ['unloadit'])
-async def unload(ctx, extension): #unloads the cog
-    bot_devs = discord.utils.get(ctx.guild.roles, id=BOT_ROLE)
+@client.command(aliases = ['unloadit'])   # Decorator that unloads cog
+async def unload(ctx, extension):   # Function that unloads cog
+    bot_devs = discord.utils.get(ctx.guild.roles, id=BOTDEV_ROLE)
     if(bot_devs in ctx.author.roles):
         try:
             client.unload_extension(f"cogs.{extension}")
-            success = f"cogs.{extension} was unloaded succesfully"
-            await ctx.channel.send(success)
-            await client.get_channel(BOT_LOGS).send(success)
-        except Exception as e: #except block to handle any exceptions encountered
-            await ctx.channel.send(e)
+            success_msg = f"cogs.{extension} was unloaded succesfully"
+            await ctx.channel.send(success_msg)
+            await client.get_channel(BOT_LOGS).send(success_msg)
+        except Exception as err: #except block to handle any exceptions encountered
+            await ctx.channel.send(err)
     else:
-        await ctx.channel.send("Unauthorised")
+        await ctx.channel.send("Unauthorised. How did you find this wtf-")
 
 for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.{filename[:-3]}") #uses string slicing to use only the file name without the extension
 
 
-client.run(TOKEN)
+client.run(BOT_TOKEN)  # Line that runs the bot
